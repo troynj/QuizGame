51fb758c895bc82727b4d7cbb50874f0adfa8840
Troy Johnson <troynjohnson6@gmail.com>
2022-12-23 10:29:24 -0800

    Touching up last commit with quiz object

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,93 +1,104 @@
-quizzArr= [
-    { //1
-        question: "Which is a coding language?",
-        answer: {
-            "Windows": false,
-            "Cascading Style Sheet": true,
-            "Index": false,
-            "Element": false
-        }
+var quizzArr = [
+  {
+    //1
+    question: "Which is a coding language?",
+    answer: {
+      Windows: false,
+      "Cascading Style Sheet": true,
+      Index: false,
+      Element: false,
     },
-    { //2
-        question: "Where do you code?",
-        answer: {
-            "On a computer": true,
-            "On a toaster": false,
-            "Under water": false,
-            "On a piece of paper": false,
-        }
+  },
+  {
+    //2
+    question: "Where do you code?",
+    answer: {
+      "On a computer": true,
+      "On a toaster": false,
+      "Under water": false,
+      "On a piece of paper": false,
     },
-    { //3
-        question: "When do you need electricity to code?",
-        answer: {
-            "never": false,
-            "sometimes": false,
-            "most of the time": false,
-            "all the time": true,
-        }
+  },
+  {
+    //3
+    question: "When do you need electricity to code?",
+    answer: {
+      never: false,
+      sometimes: false,
+      "most of the time": false,
+      "all the time": true,
     },
-    { //4
-        question: "which one of these relates to tech?",
-        answer: {
-            "banana": false,
-            "orange": false,
-            "lemon": false,
-            "apple": true,
-        }
+  },
+  {
+    //4
+    question: "which one of these relates to tech?",
+    answer: {
+      banana: false,
+      orange: false,
+      lemon: false,
+      apple: true,
     },
-    { //5
-        question: "which animal codes?",
-        answer: {
-            "caterpiller": false,
-            "bird": false,
-            "human": true,
-            "aligator": false,
-        }
+  },
+  {
+    //5
+    question: "which animal codes?",
+    answer: {
+      caterpiller: false,
+      bird: false,
+      human: true,
+      aligator: false,
     },
-    { //6
+  },
+  {
+    //6
     question: "which is a delairation operator?",
-answer: {
-    "=": true,
-    "==": false,
-    ":": false,
-    "-": false,
-}
-},
-{ //7
-question: "how do you spell Javascript?",
-answer: {
-    "jaba sript": false,
-    "java scrimp": false,
-    "javan shrimp": false,
-    "Javascript": true,
-}
-},
-{ //8
-question: "Tho created the type of computer that modern humans use today for coding purposes?",
-answer: {
-    "The Aliens": false,
-    "The Dinosaurs": false,
-    "The Gods": false,
-    "The Humans": true,
-}
-},
-{ //9
+    answer: {
+      "=": true,
+      "==": false,
+      ":": false,
+      "-": false,
+    },
+  },
+  {
+    //7
+    question: "how do you spell Javascript?",
+    answer: {
+      "jaba sript": false,
+      "java scrimp": false,
+      "javan shrimp": false,
+      Javascript: true,
+    },
+  },
+  {
+    //8
+    question:
+      "Tho created the type of computer that modern humans use today for coding purposes?",
+    answer: {
+      "The Aliens": false,
+      "The Dinosaurs": false,
+      "The Gods": false,
+      "The Humans": true,
+    },
+  },
+  {
+    //9
     question: "When coding, it is best practices to think in ____ sentences",
     answer: {
-        "illogical": false,
-        "coherient": true,
-        "incoherient": false,
-        "nonsensical": false,
-
-    }
-},
-{ //10
+      illogical: false,
+      coherient: true,
+      incoherient: false,
+      nonsensical: false,
+    },
+  },
+  {
+    //10
     question: "Coding requires which of the following: ",
     answer: {
-        "A computing instrument": true,
-        "A garden": false,
-        "A vehicle": false,
-        "A fishing pole": false,
-    }
-}
-]
+      "A computing instrument": true,
+      "A garden": false,
+      "A vehicle": false,
+      "A fishing pole": false,
+    },
+  },
+];
+
